c05639e78d81f9d0fc9c425834f7de254109494e
Ben Pettis <bpettis@gmail.com>
2026-06-19 21:05:09 -0400

    create script to clear display

---
+++ clear.py
@@ -0,0 +1,67 @@
+# SPDX-FileCopyrightText: 2019 Melissa LeBlanc-Williams for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+"""
+ePaper Display Shapes and Text demo using the Pillow Library.
+
+"""
+print("Started!")
+
+import board
+import busio
+import digitalio
+from PIL import Image, ImageDraw, ImageFont
+from adafruit_epd.epd import Adafruit_EPD
+#from adafruit_epd.uc8179 import Adafruit_UC8179
+
+from adafruit_epd.uc8253 import Adafruit_UC8253_Tricolor
+
+
+
+# First define some color constants
+WHITE = (0xFF, 0xFF, 0xFF)
+BLACK = (0x00, 0x00, 0x00)
+RED = (0xFF, 0x00, 0x00)
+
+
+# create the spi device and pins we will need
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+ecs = digitalio.DigitalInOut(board.CE0)
+dc = digitalio.DigitalInOut(board.D22)
+srcs = None
+rst = digitalio.DigitalInOut(board.D27)
+busy = digitalio.DigitalInOut(board.D17)
+
+
+
+display = Adafruit_UC8253_Tricolor(240, 416,
+    spi,
+    cs_pin=ecs,
+    dc_pin=dc,
+    sramcs_pin=srcs,
+    rst_pin=rst,
+    busy_pin=busy
+)
+
+display.rotation = 1
+
+width = display.width
+height = display.height
+image = Image.new("RGB", (width, height))
+
+
+# clear the buffer
+display.fill(Adafruit_EPD.WHITE)
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+# empty it
+draw.rectangle((0, 0, width, height), fill=WHITE)
+
+display.image(image)
+
+
+
+display.display()
+
+print("Done!")
