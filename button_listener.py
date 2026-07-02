fe905c1d056c02ef4ae8f82a44674535e2e6be29
Ben Pettis <bpettis@wisc.edu>
2026-07-01 20:24:57 -0400

    test small delay

--- button_listener.py
+++ button_listener.py
@@ -7,6 +7,7 @@ from download_calendar import main as download_calendar
 button_5 = Button(5)
 button_6 = Button(6)
 
+print("Button listener started. Waiting for button presses...")
 while True:
     if button_5.is_pressed:
         print("Button 5 is pressed")
@@ -18,11 +19,13 @@ while True:
         manual_dnd_path = Path("manual_dnd_mode")
         if manual_dnd_path.exists():
             print("Manual DND file exists. Removing it...")
+            time.sleep(0.5)
             manual_dnd_path.unlink()  # Remove the file
             update_display()
         else:
             print("Manual DND file does not exist. Creating it...")
             manual_dnd_path.touch()  # Create the file
+            time.sleep(0.5)
             update_display()
         
         with open('/sys/class/leds/ACT/brightness', 'w') as f:
